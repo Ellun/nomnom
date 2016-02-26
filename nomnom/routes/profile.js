966b29b8c60722e39db84e16d746206515d89e4a
Elton Cheng <eltoncheng86@gmail.com>
2016-02-26 16:08:14 -0500

    nutrition database is searchable

--- nomnom/routes/profile.js
+++ nomnom/routes/profile.js
@@ -1,10 +1,13 @@
 'use strict'
 
 var express     = require('express');
-var profile       = express.Router();
+var profile     = express.Router();
 var bodyParser  = require('body-parser');
-var db  = require('./../db/pg');
+var db          = require('./../db/pg');
+var request = require('request');
 
+var foodData = "https://api.nutritionix.com/v1_1/search/";
+var foodNutrition = "?results=0%3A20&cal_min=0&cal_max=50000&fields=item_name%2Cbrand_name%2Cnf_calories&appId=e7718bd7&appKey=ebfec0c4db970029e7138a6bf6e5eb65";
 
 profile.delete('/logout', function(req, res) {
   req.session.destroy(function(err) {
@@ -13,17 +16,25 @@ profile.delete('/logout', function(req, res) {
 })
 
 profile.route('/:id')
-  .get((req, res)=> {
-    res.render('../views/pages/profile', {id: req.params.id});
-  // .post()
+  .get((req, res) => {res.render('../views/pages/profile', {id: req.params.id})})
+
+profile.route('/:id/food')
+  .get((req, res) => res.render('../views/pages/food', {id: req.params.id}));
+
+
+profile.get('/:id/food/search/', getFoods, (req, res) => {
+  res.render('pages/food_search.ejs', {id: req.params.id, data: res.data})
 })
 
-profile.get('/:id/food', function(req, res){
-  res.render('../views/pages/food', {id: req.params.id});
-})
+function getFoods(req, res, next) {
+  request(foodData + req.query.item_name + foodNutrition, function(err, response, body) {
+    var data = JSON.parse(body);
+    console.log(data.hits);
+    res.data = data.hits;
+    next()
+  });
+}
 
-profile.get('/:id/programs', function(req, res){
-  res.render('../views/pages/programs', {id: req.params.id});
-})
+profile.get('/:id/programs', (req, res) => {res.render('../views/pages/programs', {id: req.params.id})})
 
 module.exports = profile;
