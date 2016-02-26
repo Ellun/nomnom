ab74069ba154f53d805ed92a32c758c56c89d936
Elton Cheng <eltoncheng86@gmail.com>
2016-02-26 16:06:32 -0500

    added profile table

--- nomnom/db/schema.sql
+++ nomnom/db/schema.sql
@@ -1,7 +1,14 @@
 DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS food_table;
 
 CREATE TABLE users (
        id SERIAL UNIQUE PRIMARY KEY,
        email VARCHAR(255),
        password_digest TEXT
 );
+
+CREATE TABLE food_table (
+      id SERIAL UNIQUE PRIMARY KEY,
+      name VARCHAR(255),
+      calories INT
+)
